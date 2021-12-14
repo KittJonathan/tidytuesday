ce2b49455a88c49dab79c53ec1fd3e7b7e5b4b0d
Jonathan Kitt <jonathan.kitt@inrae.fr>
2021-12-14 17:43:23 +0100

    update

--- R/2021_12_14_spicegirls.R
+++ R/2021_12_14_spicegirls.R
@@ -2,7 +2,6 @@
 # Date : 2021-12-14
 # Spice Girls data
 # https://github.com/rfordatascience/tidytuesday/blob/master/data/2021/2021-12-14/readme.md
-# https://peerchristensen.netlify.app/post/fair-is-foul-and-foul-is-fair-a-tidytext-entiment-analysis-of-shakespeare-s-tragedies/
 
 # Install packages ----
 
@@ -13,11 +12,7 @@
 #install.packages("tidytext")
 #install.packages("textdata")
 #install.packages("stopwords")
-#install.packages("wordcloud2")
-#install.packages("ggwordcloud")
-#install.packages("patchwork")
 #install.packages("showtext")
-#remotes::install_github("johnmackintosh/popthemes")
 
 # Load packages ----
 
@@ -25,17 +20,12 @@ library(tidytuesdayR)
 library(tidyverse)
 library(tidytext)
 library(textdata)
-library(stopwords)
-library(wordcloud2)
 library(showtext)
-library(webshot)
-library(popthemes)
-library(patchwork)
+
 
 # Import fonts ----
 
 showtext_auto()
-#font_add_google("Poiret One")
 font_add_google("Quicksand")
 
 # Import dataset ----
@@ -149,178 +139,3 @@ sentiments_plot <- ggplot(data = sentiments) +
                                     hjust = 0.5))
 
 ggsave("figs/2021_12_14_spice.png", dpi = 320, width = 12, height = 6)
-
-# Comparing minor and major modes ----
-
-modes <- tracks %>% 
-  dplyr::group_by(mode_name) %>% 
-  dplyr::summarise(mean_danceability = mean(danceability),
-                   mean_energy = mean(energy),
-                   mean_tempo = mean(tempo))
-
-modes  
-
-head(tracks)
-
-characteristics <- tracks %>% 
-  dplyr::select(id, danceability:tempo) %>% 
-  tidyr::pivot_longer(!id)
-
-ggplot(data = characteristics,
-       mapping = aes(x = id, y = name, fill = value)) +
-  geom_tile()
-  
-
-# Word clouds ----
-
-
-
-
-
-  #scale_radius(range = c(0, 20), limits = c(0, NA))
-
-wasting_my_time_words <- lyrics %>% 
-  dplyr::filter(track_name == "Wasting My Time") %>% 
-  tidytext::unnest_tokens(word, line) %>% 
-  dplyr::anti_join(tidytext::get_stopwords()) %>% 
-  dplyr::count(word, sort = T) %>% 
-  head(10) %>% 
-  ggplot(aes(label = word,
-             size = n,
-             colour = factor(sample.int(11, nrow(.), replace = TRUE)))) +
-    ggwordcloud::geom_text_wordcloud_area() +
-    scale_size_area(max_size = 50) +
-    popthemes::scale_colour_spice() +
-  ggtitle(label = "10 most used words in Wasting My Time") +
-  theme_minimal() +
-  theme(plot.background = element_rect(fill = "antiquewhite1"),
-        plot.title = element_text(family = "Quicksand",
-                                  hjust = 0.5, size = 25,
-                                  margin = margin(0, 0, 10, 0)),
-        axis.ticks.y = element_blank(),
-        panel.grid.minor = element_blank(),
-        panel.grid.major.y = element_blank(),
-        panel.grid.major.x = element_line(colour = 'grey50'),
-        axis.text = element_text(family = "Quicksand", size = 15))
-
-wasting_my_time_words
-
-viva_forever_words <- lyrics %>% 
-  dplyr::filter(track_name == "Viva Forever") %>% 
-  tidytext::unnest_tokens(word, line) %>% 
-  dplyr::anti_join(tidytext::get_stopwords()) %>% 
-  dplyr::count(word, sort = T) %>% 
-  head(10) %>% 
-  ggplot(aes(label = word,
-             size = n,
-             colour = factor(sample.int(11, nrow(.), replace = TRUE)))) +
-  ggwordcloud::geom_text_wordcloud_area() +
-  scale_size_area(max_size = 50) +
-  popthemes::scale_colour_spice() +
-  ggtitle(label = "10 most used words in Viva Forever") +
-  theme_minimal() +
-  theme(plot.background = element_rect(fill = "antiquewhite1"),
-        plot.title = element_text(family = "Quicksand",
-                                  hjust = 0.5, size = 25,
-                                  margin = margin(0, 0, 10, 0)),
-        axis.ticks.y = element_blank(),
-        panel.grid.minor = element_blank(),
-        panel.grid.major.y = element_blank(),
-        panel.grid.major.x = element_line(colour = 'grey50'),
-        axis.text = element_text(family = "Quicksand", size = 15))
-
-viva_forever_words
-
-spice_girls <- sentiments_plot / (viva_forever_words + wasting_my_time_words)
-  
-spice_girls
-
-ggsave("figs/2021_12_14_spice.png", dpi = 320, width = 12, height = 6)
-
-# Testing tidytext package ----
-
-bing_sentiments <- lyrics %>% 
-  group_by(album_name, track_number) %>% 
-
-
-valence <- tracks %>% 
-  select(album_name, track_number, valence) %>% 
-  left_join(sentiments)
-
-
-head(lyrics)
-
-lyrics %>% map_dbl(n_distinct)
-
-
-
-sentiments <- get_sentiments("nrc")
-
-wannabe_sentiments <- words %>% 
-  inner_join(sentiments) %>% 
-  count(word, sentiment, sort = T)
-
-# Plot number of species discovered each year ----
-
-p1 <- spiders %>% 
-  mutate(name = paste(genus, species, sep = " ")) %>% 
-  select(year, species = name) %>% 
-  count(year) %>% 
-  ggplot() +
-    geom_smooth(mapping = aes(x = year, y = n),
-                se = FALSE,
-                colour = "#2e8de1", size = 2) +
-    labs(x = "", y = "") +
-    labs(caption = "#TidyTuesday 2021-12-07 | World Spiders Database") +
-    ggtitle(label = "Number of spider species discovered by year") +
-    theme_minimal() +
-    theme(plot.background = element_rect(fill = "#09131C"),
-          panel.grid.minor = element_blank(),
-          panel.grid = element_line(colour = "grey30"),
-          plot.title = element_text(family = "Poiret One",
-                               size = 45,
-                               hjust = 0.5,
-                               vjust = 1,
-                               colour = "white"),
-          axis.text = element_text(family = "Poiret One",
-                                   size = 25,
-                                   colour = "white"),
-          plot.caption = element_text(colour = "white",
-                                      family = "Poiret One",
-                                      size = 20,
-                                      hjust = 0.5))
-
-# Extract data for 10 most prevalent spider families ----
-
-p2 <- spiders %>% 
-  count(family, sort = TRUE) %>% 
-  head(10) %>% 
-  ungroup() %>% 
-  mutate(family = fct_reorder(family, n)) %>% 
-  ggplot() +
-    geom_col(mapping = aes(x = family, y = n, fill = n),
-             show.legend = FALSE) +
-    scale_fill_gradient2(low = "#b65eba",
-                         high = "#2e8de1") +
-    coord_flip() +
-    labs(x = "", y = "") +
-    ggtitle(label = "10 most prevalent spider families") +
-    theme_minimal() +
-    theme(plot.background = element_rect(fill = "#09131C"),
-        panel.grid.major.y = element_blank(),
-        panel.grid.minor = element_blank(),
-        panel.grid = element_line(colour = "grey30"),
-        plot.title = element_text(family = "Poiret One",
-                                  size = 45,
-                                  hjust = 0.5,
-                                  vjust = 1,
-                                  colour = "white"),
-        axis.text = element_text(family = "Poiret One",
-                                 size = 25,
-                                 colour = "white"))
-    
-# Assemble plots and save ----
-
-plot <- p1 + p2
-
-ggsave("figs/2021_12_07_spiders.png", dpi = 320, width = 12, height = 6)
