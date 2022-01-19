79b6947397ebd80aac15b274031568bf0ec4f262
Jonathan Kitt <jonathan.kitt@inrae.fr>
2022-01-19 08:52:07 +0100

    word ratings

--- R/2022_02_18_chocolate.R
+++ R/2022_02_18_chocolate.R
@@ -9,7 +9,7 @@
 
 # Load packages ----
 
-library(ggwordcloud)
+#library(ggwordcloud)
 library(showtext)
 library(tidytuesdayR)
 library(tidyverse)
@@ -66,7 +66,7 @@ producers <- chocolate %>%
                                   n >= 15 & n < 20 ~ "15-19",
                                   n >= 20 & n < 25 ~ "20-24",
                                   n >= 25 ~ "25 +")) %>%
-    dplyr::select(region = bean_country, bin) %>% 
+    dplyr::select(region = bean_country, nb_dest = n, bin) %>% 
     dplyr::filter(region != "Blend") %>% 
     dplyr::mutate(bin = factor(bin,
                                levels = c("NA", "< 5", "5-9", "10-14",
@@ -114,21 +114,6 @@ ingredients <- rbind(ingredients, missing_ids) %>%
 
 rm(missing_ids, unique_ids, ingr_ids)
 
-ratings_nb_ingr <- ingredients %>% 
-  dplyr::group_by(nb_ingr) %>% 
-  dplyr::summarise(mean_rating = mean(rating)) %>% 
-  dplyr::filter(!is.na(nb_ingr))
-
-ggplot(data = ratings_nb_ingr) +
-  geom_col(mapping = aes(x = nb_ingr,
-                         y = mean_rating)) +
-  theme_minimal()
-
-ggplot() +
-  geom_smooth(data = ingredients,
-              mapping = aes(x = rating,
-                            y = rating))
-
 # Characteristics
 
 characteristics <- chocolate %>% 
@@ -142,36 +127,19 @@ characteristics <- chocolate %>%
   dplyr::mutate(word = str_trim(word)) %>% 
   dplyr::select(id:rating, characteristic = word)
 
-highest_rating_words <- characteristics %>% 
-  dplyr::filter(rating >= 3.0) %>% 
-  dplyr::count(characteristic, sort = T) %>% 
-  dplyr::filter(characteristic != "cocoa") %>% 
-  dplyr::top_n(10) %>% 
-  dplyr::mutate(size = 10:1)
-
-lowest_rating_words <- characteristics %>% 
-  dplyr::filter(rating <= 2) %>% 
-  dplyr::count(characteristic, sort = T) %>% 
-  dplyr::filter(!characteristic %in% c("cocoa", "very bitter")) %>% 
-  dplyr::top_n(10)
-
-ggplot(data = highest_rating_words,
-       mapping = aes(label = characteristic, size = size)) +
-  geom_text_wordcloud(area_corr = TRUE) +
-  scale_size_area(max_size = 40) +
-  theme_minimal() +
-  theme(l = element_text(family = "Poiret"))
-  
-
 # World map of producers ----
 
 world <- map_data("world") %>% 
   dplyr::filter(region != "Antarctica") %>% 
   dplyr::left_join(producers)
 
+labels <- tibble(id = 1:5,
+                 x = c(50, -80, -85, -58, -66),
+                 y = c(-26, -14, -2, 10, 22))
+
 labels <- tibble(region = c("Dominican Republic", "Ecuador", "Madagascar", "Peru", "Venezuela"),
-                 x = c(-50, -95, 72, -85, -42),
-                 y = c(23, -2, -18, -14, 9))
+                 x = c(-50, -95, 70, -85, -42),
+                 y = c(23, -2, -26, -14, 9))
 
 map <- ggplot() +
   geom_polygon(data = world,
@@ -184,12 +152,17 @@ map <- ggplot() +
   geom_text(data = labels, 
             mapping = aes(x = x, 
                           y = y,
-                          label = region),
-            family = "Poiret", colour = "black", size = 15) +
+                          label = id),
+            family = "Poiret", colour = "black", size = 12) +
   scale_fill_manual(values = c("#faf8ec", "#c28954", "#8f5431",
                                "#603217", "#420c00", "#120a08"),
                     na.value = "#b39f80", na.translate = FALSE,
                     guide = guide_legend(nrow = 1, margin = margin(0, 0, 30, 0))) +
+  annotate("text", x = -160, y = -30, label = "1 - Madagascar", size = 12, family = "Poiret", hjust = 0) +
+  annotate("text", x = -160, y = -35, label = "2 - Peru", size = 12, family = "Poiret", hjust = 0) +
+  annotate("text", x = -160, y = -40, label = "3 - Ecuador", size = 12, family = "Poiret", hjust = 0) +
+  annotate("text", x = -160, y = -45, label = "4 - Venezuela", size = 12, family = "Poiret", hjust = 0) +
+  annotate("text", x = -160, y = -50, label = "5 - Dominican Republic", size = 12, family = "Poiret", hjust = 0) +
   ggtitle(label = "Top exporters of cocoa beans",
           subtitle = "colour indicates number of destination countries") +
   theme_minimal() +
@@ -197,7 +170,6 @@ map <- ggplot() +
         axis.title = element_blank(),
         axis.text = element_blank(),
         panel.background = element_rect(fill = "#b39f80", colour = NA),
-        plot.background = element_rect(fill = "#b39f80"),
         plot.title = element_text(family = "Poiret", hjust = 0.5,
                                   colour = "white", size = 60,
                                   margin = margin(10, 0, 0, 0)),
@@ -209,35 +181,89 @@ map <- ggplot() +
         legend.spacing.x = unit(0.75, "cm"),
         legend.position = "bottom")
 
+rm(labels, world)
+
 ggsave("figs/2022_01_18_chocolate_map.png", map, dpi = 320, width = 12, height = 6)
 
 
 
 
 
-# Words describing low and high rated bars ----
+# Ratings by description words ----
 
 word_count <- characteristics %>% 
-  dplyr::count(characteristic, sort = T) %>% 
-  dplyr::filter(n > 100)
-
-word_levels <- word_count$characteristic
+  dplyr::count(characteristic, sort = TRUE) %>% 
+  dplyr::filter(n > 100) %>% 
+  dplyr::select(word = characteristic, count = n) %>% 
+  dplyr::mutate(word = fct_reorder(word, desc(count)))
 
 word_ratings <- characteristics %>% 
-  dplyr::filter(characteristic %in% word_count$characteristic) %>% 
+  dplyr::filter(characteristic %in% word_count$word) %>% 
   dplyr::group_by(characteristic) %>% 
   dplyr::summarise(min_rating = min(rating),
+                   median_rating = median(rating),
+                   mean_rating = mean(rating),
                    max_rating = max(rating)) %>% 
-  dplyr::mutate(characteristic = factor(characteristic,
-                                        levels = word_levels))
+  dplyr::select(word = characteristic, min_rating:max_rating) %>% 
+  dplyr::mutate(word = factor(word, levels = levels(word_count$word))) %>% 
+  dplyr::arrange(word) %>% 
+  tibble::add_column(id = nrow(.):1, .before = 1)
+
+add_column(id = 1:nrow(.), .before = 1)
 
 ggplot() +
   geom_segment(data = word_ratings,
                mapping = aes(x = min_rating, xend = max_rating,
-                             y = characteristic, yend = characteristic))
+                             y = word, yend = word),
+               colour = "#faf8ec", size = 1.5) +
+  geom_point(data = word_ratings,
+             mapping = aes(x = min_rating, y = word),
+             colour = "#c28954", size = 5) +
+  geom_point(data = word_ratings,
+             mapping = aes(x = max_rating, y = word),
+             colour = "#420c00", size = 5) +
+  scale_y_discrete(limits = rev) +
+  geom_segment(data = word_ratings,
+               mapping = aes(x = mean_rating, xend = mean_rating,
+                             y = id - 0.2, yend = id + 0.2),
+               size = 1, colour = "#603217") +
+  ggtitle(label = "Ratings for words most used in chocolate bars reviews ",
+          subtitle = "words shown are present in at least 100 reviews") +
+  theme_minimal() +
+  theme(axis.title = element_blank(),
+        panel.grid.minor = element_blank(),
+        panel.grid.major.y = element_blank(),
+        panel.grid.major.x = element_line(colour = "grey80", linetype = "dotted"),
+        panel.background = element_rect(fill = "#b39f80", colour = NA),
+        plot.background = element_rect(fill = "#b39f80", colour = NA),
+        plot.title = element_text(family = "Poiret", hjust = 0.5,
+                                  colour = "white", size = 50,
+                                  margin = margin(10, 0, 0, 0)),
+        plot.subtitle = element_text(family = "Poiret", hjust = 0.5,
+                                     colour = "white", size = 30,
+                                     margin = margin(5, 0, 10, 0)),
+        axis.text.x = element_text(family = "Poiret", colour = "white",
+                                   size = 15),
+        axis.text.y = element_text(family = "Poiret", colour = "white",
+                                   size = 20))
 
-  geom_segment(data = ratings,
-               mapping = aes(x = min_rating, xend = max_rating,
-                             y = characteristic, yend = characteristic))
+theme(panel.grid = element_blank(),
+      axis.title = element_blank(),
+      axis.text = element_blank(),
+      panel.background = element_rect(fill = "#b39f80", colour = NA),
+      plot.background = element_rect(fill = "#b39f80", colour = NA),
+      plot.title = element_text(family = "Poiret", hjust = 0.5,
+                                colour = "white", size = 60,
+                                margin = margin(10, 0, 0, 0)),
+      plot.subtitle = element_text(family = "Poiret", hjust = 0.5,
+                                   colour = "white", size = 50),
+      legend.title = element_blank(),
+      legend.text = element_text(family = "Poiret", colour = "black",
+                                 size = 35, margin = margin(l = -0.6, unit = "cm")),
+      legend.spacing.x = unit(0.75, "cm"),
+      legend.position = "bottom")
+
+
+
 
 # Save figs ----
