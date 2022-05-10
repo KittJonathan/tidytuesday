1ce5ac068f81a25523f67490f7b5ca784a883f8b
Jonathan Kitt <jonathan.kitt@inrae.fr>
2022-05-10 11:10:45 +0200

    update

--- R/2022_05_10_bestsellers.R
+++ R/2022_05_10_bestsellers.R
@@ -1,115 +1,80 @@
 # TidyTuesday challenge
-# Week : 12
-# Date : 2022-03-22
-# Baby names
-# https://github.com/rfordatascience/tidytuesday/blob/master/data/2022/2022-03-22/readme.md
+# Week : 19
+# Date : 2022-05-10
+# NY Times bestsellers
+# https://github.com/rfordatascience/tidytuesday/blob/master/data/2022/2022-05-10/readme.md
 
 # Load packages ----
 
 library(tidytuesdayR)
 library(tidyverse)
-# library(lubridate)
 library(showtext)
-library(patchwork)
+
+# Import fonts ----
+
+font_add_google(name = "Henny Penny", family = "Henny Penny")
+font_add_google(name = "Quando", family = "Quando")
+showtext_auto()
 
 # Import dataset ----
 
-tuesdata <- tidytuesdayR::tt_load('2022-03-22')
-
-babynames <- tuesdata$babynames
-
-rm(tuesdata)
-
-# Load fonts ----
-
-font_add_google(name = "Kranky", family = "Kranky")
-showtext_auto()
+nyt_titles <- readr::read_tsv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2022/2022-05-10/nyt_titles.tsv')
 
 # Data wrangling ----
 
-d1 <- babynames %>% 
-  mutate(code = paste(sex, name, sep = "_")) %>% 
-  filter(year %in% 1998:2017) %>% 
-  group_by(code) %>% 
-  mutate(number_years = length(year)) %>% 
-  ungroup() %>% 
-  filter(number_years == 20) %>% 
-  mutate(prop_var = var(prop)) %>% 
-  select(year, code, sex:prop, prop_var)
-
-top10_f <- d1 %>% 
-  filter(sex == "F") %>% 
-  group_by(code) %>% 
-  arrange(desc(prop_var)) %>% 
-  head(10) %>% 
-  pull(code)
-
-top10_m <- d1 %>% 
-  filter(sex == "M") %>% 
-  group_by(code) %>% 
-  arrange(desc(prop_var)) %>% 
-  head(10) %>% 
-  pull(code)
-
-table_f <- d1 %>% 
-  filter(code %in% top10_f) %>% 
-  arrange(desc(prop_var)) %>% 
-  mutate(name = factor(name, levels = rev(str_remove(top10_f, "F_"))))
-
-table_m <- d1 %>% 
-  filter(code %in% top10_m) %>% 
-  arrange(desc(prop_var)) %>% 
-  mutate(name = factor(name, levels = rev(str_remove(top10_m, "M_"))))
+d1 <- nyt_titles %>% 
+  filter(year >= 2016) %>% 
+  group_by(year) %>% 
+  arrange(desc(total_weeks), best_rank) %>% 
+  slice(1:5) %>% 
+  mutate(cumul_weeks = cumsum(total_weeks)) %>% 
+  mutate(lag.value = dplyr::lag(cumul_weeks, n = 1, default = 0)) %>% 
+  mutate(idx = factor(1:5)) %>% 
+  select(year, title, author, x.start = lag.value, x.end = cumul_weeks, idx)
 
 # Create plot ----
 
-p1 <- ggplot(table_f) +
-  geom_tile(aes(x = year, y = name, fill = prop),
-            colour = "grey40", lwd = 1, linetype = 1,
-            show.legend = FALSE) +
-  scale_fill_gradient(low = "white", high = "#45adab") +
-  scale_x_continuous(breaks = seq(1998, 2017, 4)) +
-  coord_fixed() +
-  ggtitle("Female") +
-  theme_minimal() +
-  theme(panel.background = element_rect(fill = "#9de0ad", colour = "#9de0ad"),
-        plot.background = element_rect(fill = "#9de0ad", colour = "#9de0ad"),
-        plot.title = element_text(family = "Kranky", size = 50, hjust = 0.5),
-        panel.grid = element_blank(),
-        axis.title = element_blank(),
-        axis.text = element_text(family = "Kranky", size = 35, colour = "black"))
-
-p2 <- ggplot(table_m) +
-  geom_tile(aes(x = year, y = name, fill = prop),
-            colour = "grey40", lwd = 1, linetype = 1,
-            show.legend = FALSE) +
-  scale_fill_gradient(low = "white", high = "#45adab") +
-  scale_x_continuous(breaks = seq(1998, 2017, 4)) +
-  coord_fixed() +
-  ggtitle("Male") +
-  theme_minimal() +
-  theme(panel.background = element_rect(fill = "#9de0ad", colour = "#9de0ad"),
-        plot.background = element_rect(fill = "#9de0ad", colour = "#9de0ad"),
-        plot.title = element_text(family = "Kranky", size = 50, hjust = 0.5),
-        panel.grid = element_blank(),
-        axis.title = element_blank(),
-        axis.text = element_text(family = "Kranky", size = 35, colour = "black"))
-
-p <- p1 + p2 +
-  plot_layout(ncol = 2) +
-  plot_annotation(
-    title = "The rise and fall of popular names",
-    subtitle = "(darker shades indicate higher proportions)",
-    caption = "Visualisation: Jonathan Kitt | Data source: H. Wickham | #TidyTuesday 2022 Week 12",
-    theme = theme(plot.background = element_rect(fill = "#9de0ad", colour = "#9de0ad"),
-                  panel.background = element_rect(fill = "#9de0ad", colour = "#9de0ad"),
-                  plot.title = element_text(family = "Kranky", colour = "black", size = 120, hjust = 0.5,
-                                            margin = margin(t = 5, b = 0)),
-                  plot.subtitle = element_text(family = "Kranky", colour = "black", size = 80, hjust = 0.5,
-                                            margin = margin(t = 0, b = 40)),
-                  plot.caption = element_text(colour = "black", hjust = 0.5, size = 25,
-                                              margin = margin(t = 40))))
+p <- ggplot(data = d1) +
+  geom_rect(aes(xmin = x.start, xmax = x.end, 
+                ymin = year, ymax = year + 0.7,
+                fill = idx, colour = NULL), show.legend = FALSE) +
+  scale_fill_manual(values = c("#003f5c", "#58508d", "#bc5090", "#ff6361", "#ffa600")) +
+  geom_rect(aes(xmin = 100, xmax = 120,
+                ymin = 2020.8, ymax = 2021), fill = "white") +
+  geom_text(aes(x = 110, y = 2020.9, label = "2020"), colour = "#004c6d", size = 8, family = "Quando") +
+  geom_rect(aes(xmin = 100, xmax = 120,
+                ymin = 2019.8, ymax = 2020), fill = "white") +
+  geom_text(aes(x = 110, y = 2019.9, label = "2019"), colour = "#004c6d", size = 8, family = "Quando") +
+  geom_rect(aes(xmin = 100, xmax = 120,
+                ymin = 2018.8, ymax = 2019), fill = "white") +
+  geom_text(aes(x = 110, y = 2018.9, label = "2018"), colour = "#004c6d", size = 8, family = "Quando") +
+  geom_rect(aes(xmin = 100, xmax = 120,
+                ymin = 2017.8, ymax = 2018), fill = "white") +
+  geom_text(aes(x = 110, y = 2017.9, label = "2017"), colour = "#004c6d", size = 8, family = "Quando") +
+  geom_rect(aes(xmin = 100, xmax = 120,
+                ymin = 2016.8, ymax = 2017), fill = "white") +
+  geom_text(aes(x = 110, y = 2016.9, label = "2016"), colour = "#004c6d", size = 8, family = "Quando") +
+  geom_rect(aes(xmin = 0, xmax = 220,
+                ymin = 2016, ymax = 2021),
+            fill = NA, colour = "white", size = 1.5) +
+  geom_segment(aes(x = 0, xend = 220,
+                   y = year, yend = year),
+               size = 1.5, colour = "white") +
+  geom_text(aes(x = x.start + ((x.end - x.start)/2), y = year + 0.45, label = title),
+            size = 6.5, family = "Quando", colour = "white") +
+  geom_text(aes(x = x.start + ((x.end - x.start)/2), y = year + 0.25, label = author),
+            size = 6, family = "Quando", colour = "white") +
+  labs(title = "New York Times top 5 best sellers (2016-2020)",
+       subtitle = "width represents total weeks on the best sellers list",
+       caption = "Visualisation: Jonathan Kitt | Data source: Post 45 | #TidyTuesday 2022 Week 19") +
+  theme_void() +
+  theme(panel.background = element_rect(fill = "#9dc6e0", colour = "#9dc6e0"),
+        plot.background = element_rect(fill = "#9dc6e0", colour = "#9dc6e0"),
+        plot.title = element_text(family = "Henny Penny", colour = "#004c6d", hjust = 0.5, size = 50,
+                                  margin = margin(t = 20)),
+        plot.subtitle = element_text(family = "Henny Penny", colour = "#004c6d", hjust = 0.5, size = 35),
+        plot.caption = element_text(colour = "#004c6d", hjust = 0.5, size = 20, margin = margin(b = 10)))
 
 # Save plot ----
 
-ggsave("figs/2022_03_22_babynames.png", p, dpi = 320, width = 12, height = 6)
+ggsave("figs/2022_05_10_bestsellers.png", p, dpi = 320, width = 12, height = 6)
