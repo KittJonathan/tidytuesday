2968df7181a72f1a6915b683fc18fdcf2d62ed47
Jonathan Kitt <jonathan.kitt@inrae.fr>
2026-01-13 14:53:48 +0100

    #tidytuesday W02 ok

--- 2026/tt_2026_02.R
+++ 2026/tt_2026_02.R
@@ -11,94 +11,70 @@ library(tidytuesdayR)
 library(rnaturalearth)
 library(patchwork)
 
-# Data ----
+# Import data ----
 
 tuesdata <- tidytuesdayR::tt_load(2026, week = 2)
 lang <- tuesdata$africa
 
+# List of countries qualified for the 2025 Africa Cup of Nations
+# https://en.wikipedia.org/wiki/2025_Africa_Cup_of_Nations
+
+teams <- c("Morocco", "Burkina Faso", "Cameroon", "Algeria",
+           "Congo", "Senegal", "Egypt", "Angola",
+           "Equatorial Guinea", "Ivory Coast", "Gabon", "Uganda",
+           "South Africa", "Tunisia", "Nigeria", "Mali",
+           "Zambia", "Zimbabwe", "Comoros", "Sudan",
+           "Benin", "Tanzania", "Botswana", "Mozambique")
+
+# Explore data ----
+
+# Most spoken family language for the 24 countries
+lang_fam <- lang |> 
+  filter(country %in% teams) |> 
+  summarise(total = sum(native_speakers), .by = c(country, family)) |> 
+  slice_max(order_by = total, n = 1, by = country) |> 
+  select(region = country, family)
+
+# Create map: 6 most spoken language families ----
+
+# Map of Africa
 afr <- map_data("world") |> 
   # Rename Democratic Republic of the Congo -> Congo & Swaziland -> Eswatini
   mutate(region = case_when(region == "Democratic Republic of the Congo" ~ "Congo",
                             region == "Swaziland" ~ "Eswatini",
                             .default = region)) |> 
-  # Keep countries that are in the lang dataset
-  filter(region %in% lang$country)
+  filter(region %in% lang$country) |> 
+  left_join(lang_fam) |> 
+  filter(!is.na(family))
 
 # Get complete data of African continent for map background
 afr_cont <- ne_countries(scale = "large", 
                          continent = "africa", 
                          returnclass = "sf")
 
-# Extract list of language families
-lang_fam <- lang |> 
-  distinct(family) |> 
-  arrange(family) |> 
-  pull()
-
-# Number of languages spoken in each country
-# lang_count <- lang |> 
-#   count(country) |> 
-#   rename(region = country)
-
-# afr |> 
-#   left_join(lang_count) |> 
-#   ggplot() +
-#   geom_sf(colour = "white", fill = "white") +
-#   geom_polygon(aes(x = long, y = lat, group = group, fill = n),
-#                colour = "white") +
-#   coord_fixed()
-  
-
 # Plot ----
 
-lang_maps <- list()
+p <- ggplot() +
+  geom_sf(data = afr_cont, colour = "#1d2d46", fill = "#1d2d46") +
+  geom_polygon(data = afr, 
+               aes(x = long, y = lat, group = group, fill = family),
+               colour = "#1d2d46", linewidth = 0.2) +
+  scale_fill_manual(values = c("Indo-European" = "#e69f00",
+                               "Afroasiatic" = "#009e73",
+                               "Niger–Congo" = "#0072b2")) +
+  labs(title = "Most spoken family languages",
+       subtitle = "For the 24 countries taking part in the\n2025 African Cup of Nations",
+       caption = "#TidyTuesday 2026 W02 | Jonathan Kitt") +
+  theme_void() +
+  theme(panel.background = element_rect(fill = "black", colour = "black"),
+        plot.background = element_rect(fill = "black", colour = "black"),
+        plot.title = element_text(colour = "white", hjust = 0.5, size = 18, face = "bold",
+                                  margin = margin(t = 10)),
+        plot.subtitle = element_text(colour = "white", hjust = 0.5,
+                                     margin = margin(t = 5, b = 20), size = 14),
+        plot.caption = element_text(colour = "white", hjust = 0.5,
+                                    size = 8, margin = margin(b = 10)),
+        legend.text = element_text(colour = "white"),
+        legend.position = "top")
 
-# i <- lang_fam[1]
-
-for (i in 1:length(lang_fam)) {
-  
-  afr_sub <- afr |> 
-    filter(region %in% lang$country[lang$family == lang_fam[i]])
-  
-  total_native_speakers <- lang |> 
-    filter(family == lang_fam[i]) |> 
-    summarise(total_native_speakers = sum(native_speakers)) |> 
-    pull(total_native_speakers)
-  
-  lang_maps[[i]] <- ggplot() +
-    geom_sf(data = afr_cont, colour = "white", fill = "white") +
-    geom_polygon(data = afr_sub,
-                 aes(x = long, y = lat, group = group),
-                 colour = "red", fill = "red") +
-    labs(title = glue::glue(lang_fam[i], total_native_speakers)) +
-    coord_sf(xlim = c(-25, 60)) +
-    theme_void()
-  
-}
-
-names(lang_maps) <- paste0("p", 1:17)
-
-list2env(lang_maps, envir = globalenv())
-
-patchwork::wrap_plots(lang_maps)
-
-
-
-lang |> 
-  summarise(total_native_speakers = sum(native_speakers),
-            .by = family) |> 
-  arrange(total_native_speakers)
-
-ggplot() +
-  geom_sf(data = afr_cont, colour = "white", fill = "white") +
-  geom_polygon(data = filter(afr, region == "Lesotho"),
-               aes(x = long, y = lat, group = group),
-               fill = "blue")
-
-africa |> 
-  ggplot(aes(x = long, y = lat, group = group)) +
-  geom_polygon(fill = "white", colour = "black") +
-  coord_fixed()
-
-africa_map_countries <- sort(unique(africa_map$region))
-africa_countries <- sort(unique(africa$country))
+ggsave("2026/tt_2026_02.png", p, dpi = 320, width = 12, height = 6)
