4a62faeb6542e2e10a33b7e8cee673c5c81c2efa
Jonathan Kitt <jonathan.kitt@inrae.fr>
2026-01-20 08:58:27 +0100

    #TT W03 ok

--- 2026/tt_2026_03.R
+++ 2026/tt_2026_03.R
@@ -8,6 +8,11 @@
 
 library(tidyverse)
 library(tidytuesdayR)
+library(sysfonts)
+
+font_add_google("Roboto")
+font_add_google("Orbitron")
+showtext::showtext_auto()
 # library(patchwork)
 
 # Data ----
@@ -31,22 +36,38 @@ apod <- apod |>
   drop_na(planet) |> 
   select(date, planet) |> 
   mutate(year = year(date)) |> 
-  summarise(total = n(), .by = c(year, planet))
+  summarise(total = n(), .by = c(year, planet)) |> 
+  filter(year >= 2021)
          
 # Plot ----
 
-p <- apod |> 
-  filter(year >= 2021) |> 
-  ggplot(aes(x = year, y = fct_rev(planet), fill = total)) +
-  geom_point(aes(colour = total), size = 15,
-             show.legend = FALSE) + 
-  geom_text(aes(label = total), size = 10) +
-  scale_colour_gradient2(low = "#008080", 
-                       high = "#75dad7", midpoint = 12) +
-  theme_void() +
-  theme(panel.background = element_rect(fill = "black"),
-        plot.background = element_rect(fill = "black"),
-        axis.title = element_blank(),
-        axis.text = element_text(colour = "white"))
-    
+p <- ggplot(apod) +
+  geom_point(aes(year, planet, colour = planet), 
+             size = 10, show.legend = FALSE) +
+  geom_text(aes(x = year, y = planet, label = total),
+            size = 10, family = "Orbitron") +
+  scale_colour_manual(values = c("#e2dbd8", "#d7c299", "#01c1dc", "#c24417",
+                                 "#e0946d", "#865328", "#a5cbf0", "#6094d3")) +
+  coord_radial(start = -0.2*pi, end = 0.2*pi, inner.radius = 0.5) +
+  labs(title = "Planets of the solar system",
+       subtitle = "Number of times each planet was featured as Astronomy Picture Of the Day",
+       caption = "#TidyTuesday 2026 W03 | Jonathan Kitt") +
+  theme(text = element_text(family = "Orbitron"),
+        axis.text = element_text(size = 25, colour = "white"),
+        axis.ticks = element_blank(),
+        panel.background = element_rect(colour = "black", fill = "black"),
+        panel.grid.minor = element_blank(),
+        panel.grid.major = element_line(linewidth = 0.2, linetype = "dotted",
+                                        colour = "white"),
+        plot.background = element_rect(colour = "black", fill = "black"),
+        plot.title = element_text(colour = "white", face = "bold",
+                                  size = 50, hjust = 0.5,
+                                  margin = margin(t = 15, b = 5)),
+        plot.subtitle = element_text(colour = "white",
+                                     size = 40, hjust = 0.5,
+                                     margin = margin(b = -50)),
+        plot.caption = element_text(colour = "white",
+                                    size = 25, hjust = 0.5,
+                                    margin = margin(b = 10)))
+
 ggsave("2026/tt_2026_03.png", p, dpi = 320, height = 6, width = 12)
